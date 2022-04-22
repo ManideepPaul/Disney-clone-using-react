e9f97146f794b7c371d6f0eb90e659a12519d3de
manideep <17101992m@gmail.com>
2022-04-23 01:32:09 +0530

    started working on Home Page

--- src/component/home.jsx
+++ src/component/home.jsx
@@ -4,7 +4,10 @@ import './home.css';
 const Home = () => {
     return (
         <div className='main'>
-
+            <nav className="navbar">
+                <img src="assets/images/logo.svg" alt="" />
+                <button className="btn">LOGIN</button>
+            </nav>
         </div>
     )
 }
